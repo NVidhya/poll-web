a210d1c1a04836a2282fcae4b50aaabd0e971971
Vidhya <vidhya.n@trov.co.in>
2016-06-27 16:21:53 +0530

    Update app.module.js

--- public/app.module.js
+++ public/app.module.js
@@ -4,4 +4,4 @@ var app = angular.module('pollApp', [
 	'angular-storage',
 	'angular-jwt'
 ]);
-app.value('RESTApiPath', 'http://104.199.174.185:30222/api');
+app.value('RESTApiPath', 'http://130.211.245.251:30222/api');
